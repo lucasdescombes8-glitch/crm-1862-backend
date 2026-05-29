6a4718cc8035e1be1724dd9263416bde524e4a7f
lucasdescombes8-glitch <lucas.descombes8@gmail.com>
2026-05-29 14:59:17 +0200

    test: Test utilities and helpers

---
+++ src/__tests__/utils/testHelper.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import express, { Application } from 'express';
+
+export interface TestContext {
+  app: Application;
+  token: string;
+}
+
+export const createTestApp = (): Application => {
+  const app = express();
+  app.use(express.json());
+  return app;
+};
+
+export const makeAuthHeader = (token: string) => ({
+  Authorization: Bearer {token},
+});
+
+export const mockAuthToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
